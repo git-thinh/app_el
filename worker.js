82e4f8d92b348853928cd66a22a64688a542fb80
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2018-03-27 18:50:09 +0700

    update tabs detail

--- worker.js
+++ worker.js
@@ -1,12 +1,65 @@
-﻿var _cacheSendUI = new Array;
-var _cacheSendAPI = new Array;
-var _cacheSendAPI = new Array;
-var _opend = false;
+﻿importScripts('localforage.min.js');
+
+var GRAM_READY = false;
+var SOCKET_OPEN = false;
+
 var _socket = null;
+var _port_HTTP = 0;
+var _speak_Text = '';
 
-var _portHTTP = 0;
+var _wordGramStore = localforage.createInstance({ name: "WORD_GRAM" });
+var _gramStore = localforage.createInstance({ name: "GRAM" });
+var _dicStore = localforage.createInstance({ name: "DIC" });
 
-var _speakText = '';
+_gramStore.iterate(function (_it, _key, iterationNumber) {
+    Array.from(_it.KeyWord).forEach(function (_id) { _wordGramStore.setItem(_id, _key); });
+}).then(function () {
+    GRAM_READY = true;
+}).catch(function (err) { });
+
+_socket_Init();
+self.onmessage = function (e) { var it = e.data; if (typeof it == 'string' && SOCKET_OPEN) { _speak_Text = it; _socket.send(it); } else { it.ok = false; _message_Query(it); } };
+
+function _message_Query(m) {
+    if (!GRAM_READY && !SOCKET_OPEN) {
+        m.msg = 'System connecting service...!'
+        return;
+    }
+
+    switch (m.action) {
+        case 'GRAM_ALL_KEY':
+            _gramStore.keys().then(function (keys) {
+                m.ok = true;
+                m.result = keys;
+                self.postMessage(m);
+            }).catch(function (err) { });
+            break;
+        case 'GRAM_ALL_WORD':
+            _wordGramStore.keys().then(function (keys) {
+                m.ok = true;
+                m.result = keys;
+                self.postMessage(m);
+            }).catch(function (err) { });
+            break;
+        case 'GRAM_DETAIL_BY_KEY':
+            _gramStore.getItem(m.id).then(function (value) {
+                m.ok = true;
+                m.result = value;
+                self.postMessage(m);
+            }).catch(function (err) {
+                m.msg = 'Cannot find item has key is: ' + m.id;
+                self.postMessage(m);
+            });
+            break;
+    }
+}
+
+
+
+
+
+
+
 
 function sendMessageHTTP(obj) {
     var id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
@@ -19,7 +72,7 @@ function sendMessageHTTP(obj) {
         cache.put(id, obj);
 
         console.log(id);
-        var url = 'http://localhost:' + _portHTTP;
+        var url = 'http://localhost:' + _port_HTTP;
 
         var xhr = new XMLHttpRequest();
         xhr.open('POST', url, true);
@@ -46,51 +99,49 @@ function sendMessageHTTP(obj) {
 
 function receiveMessageAPI(data) {
     var a = data.split(':');
-    var key = a[0].toString().toUpperCase();
+    var key = a[0].toString().toUpperCase(), s = '';
     switch (key) {
+        case 'GRAMMAR_LOAD':
+            if (a.length > 1) {
+                s = data.substring(key.length + 1, data.length);
+                var json = JSON.parse(s);
+                _gramStore.length().then(function (_numberOfKeys) {
+                    if (_numberOfKeys != json.length) {
+                        Array.from(json).forEach(function (_it) {
+                            _gramStore.setItem(_it.Type, _it);
+                            Array.from(_it.KeyWord).forEach(function (_id) { _wordGramStore.setItem(_id, _it.Type); });
+                        });
+                    }
+                }).catch(function (err) {
+                    console.log(err);
+                });
+            }
+            break;
         case 'HTTP_PORT':
             if (a.length > 1)
-                _portHTTP = Number(a[1]);
+                _port_HTTP = Number(a[1]);
             break;
         case 'SPEAK_FAIL':
-            _socket.send(_speakText);
+            _socket.send(_speak_Text);
             break;
         default:
-            self.postMessage(e.data);
+            self.postMessage(data);
             break;
     }
 }
 
 /* SPEECH */
 
-self.addEventListener('message', function (e) {
-    var data = e.data;
-    if (typeof data == 'string') {
-        _speakText = data;
-        _socket.send(data);
-    } else {
-        sendMessageHTTP(data);
-    }
-    //_cacheSendAPI.push(data);
-}, false);
-
-/* TIMER */
-
-setInterval(function () {
-    if (_cacheSendUI.length > 0) {
-        var data = _cacheSendUI[0];
-        _cacheSendUI.splice(0, 1);
-        self.postMessage(data);
-    }
-}, 1000);
-
-setInterval(function () {
-    if (_opend && _cacheSendAPI.length > 0) {
-        var data = _cacheSendAPI[0];
-        _cacheSendAPI.splice(0, 1);
-        _socket.send(data);
-    }
-}, 1000);
+//self.addEventListener('message', function (e) {
+//    var data = e.data;
+//    if (typeof data == 'string') {
+//        _speak_Text = data;
+//        _socket.send(data);
+//    } else {
+//        sendMessageHTTP(data);
+//    }
+//    //_cacheSendAPI.push(data);
+//}, false);
 
 /* SOCKET */
 function _socket_Init() {
@@ -99,17 +150,18 @@ function _socket_Init() {
         receiveMessageAPI(e.data);
     };
     _socket.onopen = function () {
-        _opend = true;
-        self.postMessage({ action: 'API_OPEN' });
+        SOCKET_OPEN = true;
+        _socket.send('GRAMMAR_LOAD');
+        self.postMessage({ ok: true, callback: 'SYSTEM_READY' });
     };
     _socket.onclose = function () {
-        _opend = false;
+        SOCKET_OPEN = false;
         self.postMessage({ action: 'API_CLOSE' });
     }
 }
-_socket_Init();
+
 setInterval(function () {
-    if (!_opend) {
+    if (!SOCKET_OPEN) {
         console.log('REOPEN SOCKET .....', new Date().toString());
         _socket_Init();
     }
